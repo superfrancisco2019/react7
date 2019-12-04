b16e57fa1fc32a3a0f30701a1e51f15d1dfdc65f
François Boisselier <francois.boisselier@gmail.com>
2019-12-04 11:07:02 +0100

    fetch simpson

--- src/App.js
+++ src/App.js
@@ -3,26 +3,30 @@ import './App.css';
 import FetchSimpsons from './components/FetchSimpsons'
 import axios from 'axios'
 
+
 class App extends React.Component {
   constructor(props){
     super(props)
-    this.state = {simpson : null}
+    this.state = {
+      simpson:[]}
   }
 
-  getSimpson() {
+
+  getSimpson = () => {
     axios.get('https://quests.wilders.dev/simpsons-quotes/quotes')
       .then(response => response.data)
       .then(data => {
+        console.log(data)
         this.setState({
-          simpson: data
+          simpson: data[0]
         });
     });
   }
-
+  
   render(){
     return (
       <div>
-        <FetchSimpsons simpson={this.state.simpson}/>
+        <FetchSimpsons simpson/>
         <button type="button" onClick={this.getSimpson}>Get Simpson</button>
       </div>
     );
